ea03b2b053953e0a9d14725aa2449c20a7544a6e
Camillo Camarda <ccamarda@Camardas-MacBook-Pro.local>
2017-10-18 16:22:58 +0200

    web dir

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
 $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
